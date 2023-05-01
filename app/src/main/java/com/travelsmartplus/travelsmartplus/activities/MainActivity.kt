4713a8b4602d722904b2d13f532a2ae0ecb01165
Gabriel S <gbss91@gmail.com>
2023-05-01 19:39:07 +0100

    Added fragments

--- app/src/main/java/com/travelsmartplus/travelsmartplus/activities/MainActivity.kt
+++ app/src/main/java/com/travelsmartplus/travelsmartplus/activities/MainActivity.kt
@@ -4,7 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
+import com.travelsmartplus.travelsmartplus.R
 import com.travelsmartplus.travelsmartplus.databinding.ActivityMainBinding
+import com.travelsmartplus.travelsmartplus.fragments.*
 import com.travelsmartplus.travelsmartplus.viewModels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,6 +24,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Load the BookingSearchFragment at the start of the activity
+        replaceFragment(BookingSearchFragment())
+
+        // Clear the selected item using hidden placeholder
+        binding.bottomNavigationView.selectedItemId = R.id.bnbPlaceholder
+
+        binding.mainBtn.setOnClickListener {
+            binding.bottomNavigationView.menu.setGroupCheckable(0, false, true)
+            replaceFragment(BookingSearchFragment())
+        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            binding.bottomNavigationView.menu.setGroupCheckable(0, true, true)
+            when (item.itemId) {
+                R.id.btnMenuBookings -> { replaceFragment(MyBookingsFragment())
+                    true
+                }
+                R.id.btnMenuAllBookings -> {
+                    replaceFragment(AllBookingsFragment())
+                    true
+                }
+                R.id.btnMenuUsers -> {
+                    replaceFragment(UsersFragment())
+                    true
+                }
+                R.id.btnMenuProfile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
         // Observers - observes responses and errors
         mainViewModel.isSignedIn.observe(this) { signedIn ->
             if (!signedIn) {
@@ -26,5 +64,13 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.setReorderingAllowed(true)
+        transaction.replace(R.id.mainFragmentContainer, fragment)
+        transaction.commit()
     }
 }
