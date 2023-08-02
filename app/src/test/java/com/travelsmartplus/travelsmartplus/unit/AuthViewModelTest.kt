5ef9028e97c5a1e3b786deaf9eeca7e7cff5d917
Gabriel S <gbss91@gmail.com>
2023-08-02 20:59:30 +0100

    Added new tests

--- app/src/test/java/com/travelsmartplus/travelsmartplus/unit/AuthViewModelTest.kt
+++ app/src/test/java/com/travelsmartplus/travelsmartplus/unit/AuthViewModelTest.kt
@@ -1,22 +1,122 @@
 package com.travelsmartplus.travelsmartplus.unit
 
+import androidx.lifecycle.MutableLiveData
+import com.google.android.material.textfield.TextInputEditText
+import com.travelsmartplus.travelsmartplus.data.models.Airport
+import com.travelsmartplus.travelsmartplus.data.models.requests.SignInRequest
+import com.travelsmartplus.travelsmartplus.data.models.responses.AuthResponse
+import com.travelsmartplus.travelsmartplus.data.services.AuthService
+import com.travelsmartplus.travelsmartplus.data.services.BookingService
+import com.travelsmartplus.travelsmartplus.utils.SessionManager
+import com.travelsmartplus.travelsmartplus.viewModels.AuthViewModel
+import com.travelsmartplus.travelsmartplus.viewModels.BookingViewModel
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import retrofit2.Response
 
+@ExperimentalCoroutinesApi
 class AuthViewModelTest {
 
-    @Test
-    fun signIn() {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private val testDispatcher = UnconfinedTestDispatcher()
+    private val authService: AuthService = mockk()
+    private val viewModel: AuthViewModel = AuthViewModel(authService)
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
     }
 
     @Test
-    fun signUpAndSignIn() {
+    fun `signIn should update session`() = runTest {
+
+        val signInRequest = mockk<SignInRequest>()
+        val signInResponse = mockk<Response<AuthResponse>>()
+
+        coEvery { authService.signIn(signInRequest) } returns signInResponse
+
+        viewModel.signIn(signInRequest)
+        advanceUntilIdle()
+
+        Assert.assertEquals(signInResponse, viewModel.signInResponse.value)
+
     }
 
     @Test
-    fun signUpValidation() {
+    fun `signUpValidation returns true when all fields are valid`() = runTest  {
+
+        // Mock data
+        val firstName = mockk<TextInputEditText>()
+        val lastName = mockk<TextInputEditText>()
+        val email = mockk<TextInputEditText>()
+        val companyName = mockk<TextInputEditText>()
+        val duns = mockk<TextInputEditText>()
+        val password = mockk<TextInputEditText>()
+        val confirmPass = mockk<TextInputEditText>()
+
+        every { firstName.text.toString() } returns "John"
+        every { lastName.text.toString() } returns "Smith"
+        every { email.text.toString() } returns "john@test.com"
+        every { companyName.text.toString() } returns "Company XYZ"
+        every { duns.text.toString() } returns "123456789"
+        every { password.text.toString() } returns "Pass1234!"
+        every { confirmPass.text.toString() } returns "Pass1234!"
+
+        every { firstName.error } returns null
+        every { lastName.error } returns null
+        every { email.error } returns null
+        every { companyName.error } returns null
+        every { duns.error } returns null
+        every { password.error } returns null
+        every { confirmPass.error } returns null
+
+        val result = viewModel.signUpValidation(
+            firstName,
+            lastName,
+            email,
+            companyName,
+            duns,
+            password,
+            confirmPass
+        )
+
+        assertTrue(result)
     }
 
     @Test
-    fun signInValidation() {
+    fun `signInValidation should return true when email and password are valid`() = runTest  {
+
+        // Mock data
+        val email = mockk<TextInputEditText>()
+        val password = mockk<TextInputEditText>()
+
+        every { email.text.toString() } returns "john@test.com"
+        every { password.text.toString() } returns "Pass1234!"
+
+        every { email.error } returns null
+        every { password.error } returns null
+
+        val result = viewModel.signInValidation(email, password)
+
+        assertTrue(result)
+
     }
 }
--- app/src/test/java/com/travelsmartplus/travelsmartplus/unit/BookingViewModelTest.kt
+++ app/src/test/java/com/travelsmartplus/travelsmartplus/unit/BookingViewModelTest.kt
@@ -1,74 +1,107 @@
 package com.travelsmartplus.travelsmartplus.unit
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.travelsmartplus.travelsmartplus.data.models.Airport
 import com.travelsmartplus.travelsmartplus.data.models.Booking
+import com.travelsmartplus.travelsmartplus.data.models.FlightBooking
 import com.travelsmartplus.travelsmartplus.data.models.requests.BookingSearchRequest
 import com.travelsmartplus.travelsmartplus.data.services.BookingService
+import com.travelsmartplus.travelsmartplus.data.services.BookingServiceImpl
 import com.travelsmartplus.travelsmartplus.utils.SessionManager
 import com.travelsmartplus.travelsmartplus.viewModels.BookingViewModel
+import io.mockk.Awaits
+import io.mockk.Runs
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
-import io.mockk.runs
+import io.mockk.unmockkAll
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestRule
 import retrofit2.Response
 
 @ExperimentalCoroutinesApi
 class BookingViewModelTest {
 
-    private lateinit var viewModel: BookingViewModel
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private val testDispatcher = UnconfinedTestDispatcher()
     private val bookingService: BookingService = mockk()
     private val sessionManager: SessionManager = mockk()
-
-    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+    private lateinit var viewModel: BookingViewModel
 
     @Before
     fun setUp() {
-        Dispatchers.setMain(mainThreadSurrogate)
+        Dispatchers.setMain(testDispatcher)
+        coEvery { bookingService.getAllAirports() } coAnswers {
+            val mockedAirports = mockk<List<Airport>>()
+            Response.success(mockedAirports)
+        }
         viewModel = BookingViewModel(bookingService, sessionManager)
+
+    }
+
+    @Test
+    fun `airportsAutoComplete success returns all airports`() = runTest {
+
+        val airportsList = mockk<List<Airport>>()
+        coEvery { bookingService.getAllAirports() } returns Response.success(airportsList)
+
+        viewModel.airportsAutoComplete()
+
+        assertEquals(airportsList, viewModel.airports.value)
+    }
+
+    @Test
+    fun `bookingSearch returns booking`() = runTest {
+
+        val bookingSearchRequest = mockk<BookingSearchRequest>()
+        val booking = mockk<Booking>()
+        every { bookingSearchRequest.userId } returns 1
+        coEvery { sessionManager.currentUser() } returns 1
+        coEvery { bookingService.bookingSearch(bookingSearchRequest) } returns Response.success(booking)
+
+        viewModel.bookingSearch()
+
+        assertEquals(booking, viewModel.booking.value)
+    }
+
+    @Test
+    fun `getFlightOffers returns list of flights`() =  runTest {
+
+        val bookingSearchRequest = mockk<BookingSearchRequest>()
+        val flightOffers = mockk<List<FlightBooking>>()
+        every { bookingSearchRequest.userId } returns 1
+        coEvery { sessionManager.currentUser() } returns 1
+        coEvery { bookingService.getFlightOffers(bookingSearchRequest) } returns Response.success(flightOffers)
+
+        // Act
+        viewModel.setBookingSearchRequest(bookingSearchRequest)
+        viewModel.getFlightOffers()
+        advanceUntilIdle()
+
+        // Assert
+        assertEquals(flightOffers, viewModel.flightOffers.value)
     }
 
     @After
     fun tearDown() {
-        Dispatchers.resetMain() // reset the main dispatcher to the original Main dispatcher
-        mainThreadSurrogate.close()
+        unmockkAll()
+        Dispatchers.resetMain()
     }
-
-    @Test
-    fun `bookingSearch should return updated LiveData`() = runBlocking {
-
-        // Given...
-        val mockBooking = mockk<Booking>()
-        val mockResponse = Response.success(mockBooking)
-        val mockAirports = Response.success(mockk<List<Airport>>())
-        val bookingSearchRequest = mockk<BookingSearchRequest>()
-        val bookingService = mockk<BookingService>()
-        val sessionManager = mockk<SessionManager>()
-        val viewModel = BookingViewModel(bookingService, sessionManager)
-
-        coEvery { bookingService.getAllAirports() } returns mockAirports
-        coEvery { bookingSearchRequest.userId = any() } just runs
-        coEvery { bookingService.bookingSearch(any()) } returns mockResponse
-        coEvery { sessionManager.currentUser() } returns 1
-
-        // When...
-        viewModel.bookingSearch()
-
-        //Then...
-        assertEquals(mockBooking, viewModel.booking.value)
-        assertEquals(false, viewModel.isLoading.value)
-
-
-    }
-
 }
